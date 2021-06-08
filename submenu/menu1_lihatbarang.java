a30b9fe67d5873f6c88c551a9a78c081350f0d45
CarloAgastya <74575801+CarloAgastya@users.noreply.github.com>
2021-06-08 09:49:49 +0700

    Update menu1_lihatbarang.java

--- submenu/menu1_lihatbarang.java
+++ submenu/menu1_lihatbarang.java
@@ -3,6 +3,8 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+//untuk melihat tampilan barang-barang yang tersedia
+
 package submenu;
 
 import java.sql.Connection;
